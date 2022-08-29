a94e3a2a8968cf4126b60cfb086f040095f8578e
mcnic16 <jamesmcnic@yahoo.co.uk>
2022-08-29 22:34:05 +0000

    Wprking on starters database but can't get it to return

--- glutenfree/routes.py
+++ glutenfree/routes.py
@@ -12,34 +12,34 @@ def home():
 def cuisine():
     return render_template("cuisine.html")
 
+@app.route("/starters")
+def starters():
+    starters = list(Starter.query.order_by(Starter.id).all())
+    return render_template("starters.html", starters=starters)
+
 
 @app.route("/add_cuisine")
 def add_cuisine():
     return render_template("add_cuisine.html")
 
 
-@app.route("/starters")
-def starters():
-    starters = list(Starter.query.order_by(Starter.id).all())
-    return render_template("starters.html")
 
 
-@app.route("/add_starters")
+@app.route("/add_starters",  methods=["GET", "POST"])
 def add_starters():
     if request.method == "POST":
         starter = Starter(
-            starter_names = request.form.get("starter_names"),
-            starter_tools = request.form.get("starter_tools"),
-            starter_ingredients = request.form.get("starter_ingredients"),
-            starter_directions = request.form.get("starter_directions"),
+            starter_names=request.form.get("starter_names"),
+            starter_tools=request.form.get("starter_tools"),
+            starter_ingredients=request.form.get("starter_ingredients"),
+            starter_directions=request.form.get("starter_directions")
         )
-
         db.session.add(starter)
         db.session.commit()
         return redirect(url_for("starters"))
-    return render_template("add_starters.html")
+    return render_template("add_starters.html" )
 
-        
+
 
 
 
