cded98731263edbb3ca2d95b6053cef8d88254ef
mcnic16 <jamesmcnic@yahoo.co.uk>
2022-09-02 17:47:26 +0000

    debugging

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ from glutenfree import app
 if __name__ == "__main__":
     app.run(
         #host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=os.environ.get("DEBUG")
+        #port=int(os.environ.get("PORT")),
+       # debug=os.environ.get("DEBUG")
     )
