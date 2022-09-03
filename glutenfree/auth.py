fa21df49e926930f7c3a75cac4053dcc1545f1e4
mcnic16 <jamesmcnic@yahoo.co.uk>
2022-09-03 13:19:21 +0000

    updated auth.py file sign up not working

--- glutenfree/auth.py
+++ glutenfree/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
+from werkzeug.security import generate_password_hash, check_password_hash
 from glutenfree import db
 
 auth = Blueprint("auth", __name__)
@@ -7,9 +8,26 @@ auth = Blueprint("auth", __name__)
 def login():
     return render_template('login.html')
 
-@auth.route("/register")
+
+@auth.route("register")
 def register():
     return render_template('register.html')
+
+
+auth.route("/register", methods=['POST'])
+def register_post():
+    #  to validate and add user to database
+    name = request.form.get('name')
+    password = request.form.get('password')
+    user = User.query.filter_by(name=name).first()
+    if user: 
+        return redirect(url_for('auth.register'))
+    new_user = User(name=name, password=generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
 
 @auth.route("/logout")
 def logout():
